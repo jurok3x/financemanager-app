f77aba4e4e891bcbcfe4a7123f54b9917798ecb5
Юра <@My_PC>
2021-09-14 15:44:17 +0300

    Role generation strategy -> identity

--- src/main/java/com/financemanager/demo/site/entity/Role.java
+++ src/main/java/com/financemanager/demo/site/entity/Role.java
@@ -21,7 +21,7 @@ import lombok.Setter;
 @Setter
 public class Role {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	@Column(name="name")
