6ada59f6db90eb28ce5f3505fdde069064ce93ae
Юра <@My_PC>
2021-09-15 13:31:16 +0300

    new try

--- src/main/resources/static/js/save_user.js
+++ src/main/resources/static/js/save_user.js
@@ -1,17 +1,10 @@
 function createUser() {
-        let userName = document.getElementById("name").value;
-        let userLogin = document.getElementById("login").value;
-        let userPassword = document.getElementById("password").value;
-        let userEmail = document.getElementById("email").value;
-        alert("User SAVED");
-        const  response = await fetch("https://myapp-12344.herokuapp.com/api/users", {
-			method:
-				"POST",
-			body:
-				JSON.stringify({name: userName, login: userLogin, password: userPassword, email:userEmail}),
-			headers: {
- 	  	  		'Content-Type': 'application/json'
-  		  	}  	  	
-  		  });
-  		return await response.json();
+        var userName = document.getElementById("name").value;
+        var userLogin = document.getElementById("login").value;
+        var userPassword = document.getElementById("password").value;
+        var userEmail = document.getElementById("email").value;
+        var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+        xmlhttp.open("POST", "https://myapp-12344.herokuapp.com/api/users");
+        xmlhttp.setRequestHeader("Content-Type", "application/json");
+        xmlhttp.send(JSON.stringify({name: userName, login: userLogin, password: userPassword, email:userEmail}));
     }
