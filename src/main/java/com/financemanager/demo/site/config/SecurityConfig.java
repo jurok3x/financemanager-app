03441f38eacfb3905c453640c473a5603a9cc6aa
Юра <@My_PC>
2021-09-17 14:10:06 +0300

    New try2

--- src/main/java/com/financemanager/demo/site/config/SecurityConfig.java
+++ src/main/java/com/financemanager/demo/site/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                
+                .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                 .and()
                 	.authorizeRequests()
