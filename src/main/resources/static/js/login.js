6b8ddfb861b24e0c2b59c811d8ce34775044e8ea
Юра <@My_PC>
2021-09-22 15:43:54 +0300

    Next try

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,6 +1,7 @@
 function logIn(){
 	const userLogin = document.getElementById('login').value;
 	const userPassword = document.getElementById('password').value;
+	alert(userLogin + userPassword);
 	const  response = await fetch("https://myapp-12344.herokuapp.com/api/auth", {
 			method: "POST",
 			body: JSON.stringify({ login: userLogin, password: userPassword }),
