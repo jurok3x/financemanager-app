10b284e89dd8aa83b3c01b5a97c65b21429286cb
Юра <@My_PC>
2021-09-27 13:23:22 +0300

    Saving version before rebasing to another branch

--- src/com/financemanager/demo/site/entity/projects/ProjectNameAndCountAndCost.java
+++
@@ -1,7 +0,0 @@
-package com.financemanager.demo.site.entity.projects;
-
-public interface ProjectNameAndCountAndCost {
-    String getItem_Name();
-    Integer getValue();
-    Double getTotal();
-}
