11d3c962b7e5754ce6e49e2bbc57f7abd136937c
Юра <@My_PC>
2021-10-06 15:53:45 +0300

    added allowed headers

--- src/main/java/com/financemanager/demo/site/config/SecurityConfig.java
+++ src/main/java/com/financemanager/demo/site/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			return new WebMvcConfigurer() {
 				@Override
 				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**").allowedOrigins("*");
+					registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*");
 				}
 			};
 		}
