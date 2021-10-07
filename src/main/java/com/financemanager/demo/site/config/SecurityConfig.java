878e35f881e464e80bc1f1a1ef00ec4bd19a5459
Юра <@My_PC>
2021-10-07 14:29:17 +0300

    Exposed headers

--- src/main/java/com/financemanager/demo/site/config/SecurityConfig.java
+++ src/main/java/com/financemanager/demo/site/config/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				public void addCorsMappings(CorsRegistry registry) {
 					registry.addMapping("/**")
 					.allowedOrigins("*")
-					.allowedHeaders("*");
+					.allowedHeaders("*")
+					.exposedHeaders("*");
 				}
 			};
 		}
