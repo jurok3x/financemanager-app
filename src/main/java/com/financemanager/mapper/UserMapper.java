b9316ac6ad069c39fc847878c5ac79a46d3b420b
ykotsiuba <jurok3x@gmail.com>
2022-09-01 15:07:42 +0300

    Fixed user mapper

--- src/main/java/com/financemanager/mapper/UserMapper.java
+++ src/main/java/com/financemanager/mapper/UserMapper.java
@@ -9,21 +9,25 @@ import org.springframework.stereotype.Component;
 
 import java.util.stream.Collectors;
 
-
 @AllArgsConstructor
 @Component
 public class UserMapper {
-    
+
     private RoleMapper roleMapper;
     private CategoryMapper categoryMapper;
-    
+
     public UserDTO toUserDTO(User user) {
         UserDTO userDTO = new UserDTO();
         userDTO.setId(user.getId());
         userDTO.setName(user.getName());
         userDTO.setEmail(user.getEmail());
-        userDTO.setRoleDTO(roleMapper.toRoleDTO(user.getRole()));
-        userDTO.setCategories(user.getCategories().stream().map(categoryMapper::toCategoryDTO).collect(Collectors.toSet()));
+        if (user.getRole() != null) {
+            userDTO.setRoleDTO(roleMapper.toRoleDTO(user.getRole()));
+        }
+        if (user.getCategories().size() != 0) {
+            userDTO.setCategories(
+                    user.getCategories().stream().map(categoryMapper::toCategoryDTO).collect(Collectors.toSet()));
+        }
         return userDTO;
     }
 
@@ -32,7 +36,9 @@ public class UserMapper {
         user.setId(userDTO.getId());
         user.setName(userDTO.getName());
         user.setEmail(userDTO.getEmail());
-        user.setRole(roleMapper.toRole(userDTO.getRoleDTO()));
+        if (userDTO.getRoleDTO() != null) {
+            user.setRole(roleMapper.toRole(userDTO.getRoleDTO()));
+        }
         return user;
     }
 
